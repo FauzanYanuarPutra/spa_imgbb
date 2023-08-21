a37b67747483e0914b6e2a408a77f0bab0983c1a
Fauzanyan <ccdplayerx@gmail.com>
2023-08-21 19:29:32 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,168 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <a
+      href="https://u8486.imgbb.com/"
+      target="_blank"
+      class="my-5 underline text-blue-500"
+      >Lihat Semua Postingan Disini</a
+    >
+    <div
+      class="border-dashed border-2 border-gray-300 p-4 text-center cursor-pointer w-[80%] m-auto"
+      @dragover.prevent
+      @drop="handleDrop"
+      @click="openFileInput"
+    >
+      <p class="text-gray-700 text-lg mb-2">Drag & Drop or Click to Add Image</p>
+      <input type="file" @change="handleFileUpload" class="hidden" ref="fileInput" />
+      <div v-if="selectedFile" class="mt-4">
+        <img
+          :src="previewImageUrl"
+          alt="Preview Image"
+          class="max-w-full m-auto w-[400px]"
+        />
+      </div>
+    </div>
+
+    <button
+      class="mt-4 bg-blue-500 text-white px-4 py-2 rounded flex items-center mx-auto justify-center"
+      @click="uploadImage"
+      :disabled="isUploading"
+    >
+      <p v-if="isUploading" class="animate-spin mr-2">&#9696;</p>
+      Upload Image
+    </button>
+
+    <div class="my-8">
+      <p v-if="uploadStatus === 'success'" class="text-green-700">
+        {{ messages["success"] }}
+        <a :href="uploadedImageUrl" target="_blank" class="underline">Lihat Di ImgBB</a>
+      </p>
+      <p
+        v-else-if="
+          uploadStatus === 'error' ||
+          uploadStatus === 'invalidFormat' ||
+          uploadStatus === 'fileTooLarge'
+        "
+        class="text-red-700"
+      >
+        {{ messages[uploadStatus] }}
+      </p>
+    </div>
+
+    <h2 class="mt-10">Gambar Yang Di Upload</h2>
+
+    <div class="grid gap-2 mx-auto w-[80%]">
+      <div v-for="(url, index) in uploadedImageUrls" :key="index" class="mx-auto">
+        <a :href="url" target="_blank">
+          <img
+            :src="url"
+            alt="Uploaded Image"
+            style="margin: 10px"
+            class="w-[300px] h-[300px] object-cover"
+          />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      selectedFile: null,
+      apiKey: process.env.VUE_APP_API_KEY,
+      uploadStatus: null,
+      uploadedImageUrl: "",
+      isUploading: false,
+      uploadedImageUrls: [],
+      messages: {
+        success: "Gambar berhasil diupload. ",
+        invalidFormat: "Hanya boleh file gambar (jpg, jpeg, bmp, png, gif)",
+        fileTooLarge: "File gambar terlalu besar. Max ukuran file 2MB.",
+        error: "Upload error",
+      },
+    };
+  },
+  created() {
+    this.loadUploadedImagesFromLocalStorage();
+  },
+  methods: {
+    handleDrop(event) {
+      event.preventDefault();
+      this.selectedFile = event.dataTransfer.files[0];
+    },
+    openFileInput() {
+      this.$refs.fileInput.click();
+    },
+    handleFileUpload(event) {
+      this.selectedFile = event.target.files[0];
+    },
+    uploadImage() {
+      if (!this.selectedFile) return;
+
+      if (!this.isValidImageFormat(this.selectedFile)) {
+        this.uploadStatus = "invalidFormat";
+        return;
+      }
+
+      if (!this.isValidImageSize(this.selectedFile)) {
+        this.uploadStatus = "fileTooLarge";
+        return;
+      }
+
+      this.isUploading = true;
+
+      const formData = new FormData();
+      formData.append("key", this.apiKey);
+      formData.append("image", this.selectedFile);
+
+      axios
+        .post("https://api.imgbb.com/1/upload", formData)
+        .then((response) => {
+          this.uploadedImageUrl = response.data.data.url;
+          this.uploadStatus = "success";
+          this.saveImageUrlToLocalStorage(response.data.data.url);
+        })
+        .catch(() => {
+          this.uploadStatus = "error";
+        })
+        .finally(() => {
+          this.isUploading = false;
+        });
+    },
+
+    isValidImageFormat(file) {
+      const allowedFormats = ["image/jpeg", "image/png", "image/bmp", "image/gif"];
+      return allowedFormats.includes(file.type);
+    },
+    isValidImageSize(file) {
+      return file.size <= 2 * 1024 * 1024;
+    },
+    saveImageUrlToLocalStorage(url) {
+      const existingUrls = JSON.parse(localStorage.getItem("uploadedImageUrls")) || [];
+      existingUrls.push(url);
+      localStorage.setItem("uploadedImageUrls", JSON.stringify(existingUrls));
+      this.loadUploadedImagesFromLocalStorage();
+    },
+    loadUploadedImagesFromLocalStorage() {
+      this.uploadedImageUrls =
+        JSON.parse(localStorage.getItem("uploadedImageUrls")) || [];
+    },
+  },
+  computed: {
+    previewImageUrl() {
+      return this.selectedFile ? URL.createObjectURL(this.selectedFile) : null;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
 }
 </style>
